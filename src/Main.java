65ee54cdcc30178b859f702e6c833004cb15c5b2
Lucashmcastro <lucashmcastro@outlook.com>
2021-04-18 10:16:41 -0300

    Change the main

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,32 @@
 import java.util.Scanner;
-
 public class Main {
 
 	public static void main(String[] args) {
 
-		 Scanner input = new Scanner(System.in); //input para receber as informaçoes digitadas pelo usuario
+		 Scanner input = new Scanner(System.in); //input para receber as informaÃ§oes digitadas pelo usuario
 		
 		 System.out.println("Digite a capacidade da fila de tarefas:"); //recebendo a capacidade da fila e enviando para maquina
 		 int tamFila = input.nextInt();
 		 
 		 Fila fila = new Fila(tamFila); //Criando nossa fila de tarefas 			 	
 		
-		 System.out.println("Digite o número de máquinas :");	//recebendo a quantidade de maquinas  	 
+		 System.out.println("Digite o numero de maquinas :");	//recebendo a quantidade de maquinas  	 
 		 int contMaquinas = input.nextInt();
-		 Maquina [] idMaquinas = new Maquina[contMaquinas]; // atribuindo a quantidade de espaço a ser reservado para as maquinas
+		 Maquina [] idMaquinas = new Maquina[contMaquinas]; // atribuindo a quantidade de espaÃ§o a ser reservado para as maquinas
 		 String [] idMachine = new String[contMaquinas];
 		 
-		 for (int i = 0 ; i < contMaquinas; i++) { // laço que cria as maquinas
-			 System.out.println ( "Digite o identificador da máquina " + (i+1) + ":");
-			  idMachine[i] = input.next();
+		 for (int i = 0 ; i < contMaquinas; i++) { // laÃ§o que cria as maquinas
+			 System.out.println ( "Digite o identificador da maquina " + (i+1) + ":");
+			   idMachine[i] = input.next();
 			   idMaquinas[i] = new Maquina (idMachine[i],fila);// enviando por parametro o id da maquina e fila;
 			   idMaquinas[i].start();		 
 		 }		 
 		 
-		 System.out.println("Digite o número de funcionários:");
+		 System.out.println("Digite o numero de funcionÃ¡rios:");
 		 int contFunc = input.nextInt();
 		 Funcionario [] qtdFunc = new Funcionario[contFunc];
 		 
-		 for( int x = 0 ; x < contFunc; x++) { //laço que cria os funcionarios
+		 for( int x = 0 ; x < contFunc; x++) { //laÃ§o que cria os funcionarios
 			 qtdFunc[x] = new Funcionario (fila,"func"+(x+1), x+1);// enviando por paramentro a fila, o arquivo txt e o id do funcionario
 			 qtdFunc[x].start();
 		 }	
@@ -35,7 +34,7 @@ public class Main {
 		 boolean aux = false;
 		 
 		 while (aux == false) {
-			 if(fila.Verificar() == true && qtdFunc[contFunc-1].Verificar() == true ) {// VAI ENTRA NA CONDIÇÃO DE A FILA ESTIVER VAZIA E OS FUNCIONARIOS FINALIZARAM.
+			 if(fila.Verificar() == true && qtdFunc[contFunc-1].Verificar() == true ) {
 				 for(int y=0 ; y < contMaquinas; y++) {
 					 System.out.println(idMachine[y]+" FINALIZADA !");
 				 }	
@@ -45,3 +44,5 @@ public class Main {
 		 
 	}
 }
+
+
